ff726531bfb0ea43c39fecbfbe78c214f69919d2
C5543 <cadyrizini@gmail.com>
2025-05-07 18:24:19 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -170,7 +170,7 @@ with st.sidebar:
         st.button("Delete Chat", on_click=delete_chat)
 
 # Main Content
-st.title("Chatbot Application")
+st.title("Chatbot Application Cady terraform")
 
 if st.session_state["current_chat"]:
     chat_id = st.session_state["current_chat"]
